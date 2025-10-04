85b2214cf2d179cdafcec3957012b8f4fc83c2d3
fathimarinzy <fathimarinzy02@gmail.com>
2025-10-04 17:22:41 +0530

    seventhieth commit

--- backend/routes/auth.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request
-from auth import login
-
-auth_bp = Blueprint('auth', __name__)
-
-@auth_bp.route('/api/login', methods=['POST'])
-def auth_login():
-    return login()
